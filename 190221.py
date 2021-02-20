df24c4b48dfbe1b30b626eb122724a0bd77885b0
alinkak311 <71321784+alinkak311@users.noreply.github.com>
2021-02-20 12:46:36 +0700

    Create 190221.py

---
+++ 190221.py
@@ -0,0 +1,23 @@
+66а
+print("vvedite 5 slov:")
+def uou(n):
+    gg=[]
+    while len(n)<5:
+        lok=input()
+        if lok=='':
+            break
+        lol.append(bet)
+    return (gg)
+def x(a):
+    s=[]
+    for i in a:
+        i=i[2:]
+        s.append(i)
+    return(s)
+m=[]
+m=uou(m)
+m=x(m)
+c=sorted(m)
+print("spisok alfavit :")
+for i in c:
+    print(i,end=' ')
